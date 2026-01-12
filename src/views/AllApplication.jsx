92a4f640fb07d8a28c7a3ec079b68cef59bd89f8
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-12 18:06:15 +0800

    Create file for application view

---
+++ src/views/AllApplication.jsx
@@ -0,0 +1,13 @@
+import ApplicationList from "../components/ApplicationList";
+import ApplicationItem from "../components/ApplicationItem";
+
+const AllApplication = () => {
+  return (
+    <>
+      <ApplicationList />
+      <ApplicationItem />
+    </>
+  );
+};
+
+export default AllApplication;
