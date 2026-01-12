0114ec308ce3f915a5e0e57e3601d20825526438
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-12 19:25:04 +0800

    Modify text style

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
-  <header className="text-white p-4 text-center ">
-    <h1 className="text-2xl font-bold">Job Application Tracker</h1>
+  <header className="text-gray-700 p-4 text-center ">
+    <p className="text-4xl font-bold">Job Application Tracker</p>
   </header>
 );
 export default Header;
