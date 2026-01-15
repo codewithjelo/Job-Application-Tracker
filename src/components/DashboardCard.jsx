caed2a2122d7de6b31794af9811af0872c1ff8e2
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-15 16:03:09 +0800

    Add dashboard cards with total applications and applied count

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,9 +1,31 @@
+import { APPLICATIONS } from "../data/applications";
+
 const DashboardCard = () => {
-    return (
-    <>
-    
-    </>
-    );
-}
+  const cardList = [
+    {
+      title: "Total Job Application",
+      count: APPLICATIONS.length,
+    },
+    {
+      title: "Total Applied",
+      count: APPLICATIONS.filter((app) => app.status === "Applied").length,
+    },
+  ];
+
+  return (
+    <div className="flex flex-row">
+      {cardList.map((app) => (
+      <div className="flex flex-col p-4 border border-[var(--border)]">
+        <p className="text-xl font-semibold text-[var(--primary)]">
+          {app.title}
+        </p>
+        <span className="text-xl font-semibold text-[var(--secondary)]">
+          {app.count}
+        </span>
+      </div>
+      ))};
+    </div>
+  );
+};
 
 export default DashboardCard;
