8434e8effdb8b73070d42f4e0621347560f8c615
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-15 16:01:21 +0800

    Import dashboard card component

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,6 +1,10 @@
+import DashboardCard from "../components/DashboardCard";
+
 const Dashboard = () => {
     return (
-        <></>
+        <div>
+            <DashboardCard />
+        </div>
     );
 }
 
