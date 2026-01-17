76041076bcb62923273f9cd2e90549d8840b55e4
Jelo <jannangelodimaano@gmail.com>
2026-01-17 23:40:00 +0800

    Import recent activity panel

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,11 +1,17 @@
 import DashboardCard from "../components/DashboardCard";
+import RecentActivity from "../components/RecentActivity";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <DashboardCard />
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="bg-blue">
+        <DashboardCard />
+      </div>
+      <div className="bg-red">
+        <RecentActivity />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
