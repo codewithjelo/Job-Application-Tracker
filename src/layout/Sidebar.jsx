408cd38a832b876cae16e18ffd3664de3aebdb7e
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-19 21:09:37 +0800

    Add icon each link

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useState } from "react";
+import {
+  LayoutDashboard,
+  Briefcase,
+  PlusCircle,
+  ClipboardList,
+} from "lucide-react";
 import "../styles/layout/sidebar.css";
 
-const Sidebar = ({ onCategoryClick }) => {  
+const Sidebar = ({ onCategoryClick }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => setIsOpen(!isOpen);
@@ -25,46 +31,52 @@ const Sidebar = ({ onCategoryClick }) => {
       {/* Sidebar */}
       <div
         className={`fixed top-0 left-0 h-full w-64 text-white transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
+          isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 md:translate-x-0 md:w-64 z-40`}
       >
         <div className="p-4">
-          <h2 className="text-4xl text-center text-gray-700 font-bold mb-4">J.A.T.</h2>
+          <h2 className="text-4xl text-center text-gray-700 font-bold mb-4">
+            J.A.T.
+          </h2>
           <nav>
             <ul className="space-y-2">
               <li>
                 <a
                   href="#"
-                  onClick={(e) => handleClick(e, 'dashboard')}
-                  className="block p-2"
+                  onClick={(e) => handleClick(e, "dashboard")}
+                  className="block p-2 rounded-md text-md mb-4 text-[var(--primary)] font-semibold flex items-center gap-2"
                 >
+                  <LayoutDashboard className="w-5 h-5" />
                   Dashboard
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  onClick={(e) => handleClick(e, 'all-applications')}
-                  className="block p-2"
+                  onClick={(e) => handleClick(e, "all-applications")}
+                  className="block p-2 rounded-md text-md mb-4 text-[var(--primary)] font-semibold flex items-center gap-2"
                 >
+                  <Briefcase className="w-5 h-5" />
                   All Applications
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  onClick={(e) => handleClick(e, 'add-new')}
-                  className="block p-2"
+                  onClick={(e) => handleClick(e, "add-new")}
+                  className="block p-2 rounded-md text-md mb-4 text-[var(--primary)] font-semibold flex items-center gap-2"
                 >
+                  <PlusCircle className="w-5 h-5" />
                   Add New Application
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  onClick={(e) => handleClick(e, 'activity-log')}
-                  className="block p-2"
+                  onClick={(e) => handleClick(e, "activity-log")}
+                  className="block p-2 rounded-md text-md mb-4 text-[var(--primary)] font-semibold flex items-center gap-2"
                 >
+                  <ClipboardList className="w-5 h-5" />
                   Job Application Log
                 </a>
               </li>
