3ed03e1d936c6bcead10f83d68e87a8dee8d0f94
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-19 12:28:54 +0800

    Update dashboard layout using flex for trend and activity sections

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,14 +1,18 @@
 import DashboardCard from "../components/DashboardCard";
+import ApplicationTrend from "../components/ApplicationTrend";
 import RecentActivity from "../components/RecentActivity";
 
 const Dashboard = () => {
   return (
     <div className="flex flex-col gap-5">
-      <div className="bg-blue">
-        <DashboardCard />
-      </div>
-      <div className="bg-red">
-        <RecentActivity />
+      <DashboardCard />
+      <div className="flex flex-row gap-5">
+        <div className="flex-1">
+          <ApplicationTrend />
+        </div>
+        <div className="flex-1">
+          <RecentActivity />
+        </div>
       </div>
     </div>
   );
