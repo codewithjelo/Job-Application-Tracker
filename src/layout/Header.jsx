662ca8c25d61e5c48743adaa0a118c8f3b40b682
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-21 20:16:49 +0800

    Change text color

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <header className="text-gray-700 p-4 text-center ">
+  <header className="text-[var(--primary)] p-4 text-center ">
     <p className="text-4xl font-bold">Job Application Tracker</p>
   </header>
 );
