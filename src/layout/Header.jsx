e45c93a6ff8f3b610ddb72cb7c1829ec7e21dcb1
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-23 18:28:01 +0800

    Remove unecessary space

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <header className="text-[var(--primary)] p-4 text-center ">
+  <header className="text-[var(--primary)] p-4 text-center">
     <p className="text-4xl font-bold">TrabaLog</p>
   </header>
 );
