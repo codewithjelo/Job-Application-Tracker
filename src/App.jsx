99bd1f50dd28e037dab35fe74fb0ae6db1b45609
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-23 14:17:32 +0800

    Add w-screen, set width to full and add minimum width 0 for header and content area

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   };
 
   return (
-    <div className="flex min-h-screen overflow-hidden">
+    <div className="flex h-screen w-screen max-w-full overflow-x-hidden">
       <Sidebar onCategoryClick={handleCategoryClick} />
 
-      <div className="flex flex-col flex-1 md:ml-64">
+      <div className="flex flex-col flex-1 md:ml-64 min-w-0">
         <Header />
         <div className="p-5 flex-1">
           <ContentArea
