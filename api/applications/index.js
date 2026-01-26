0497962271c17e3b93650c734e227acca7dd781c
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-26 14:06:40 +0800

    Restore recent api handler

--- api/applications/index.js
+++ api/applications/index.js
@@ -1,19 +1,34 @@
 const applications = require("../data/applications");
 
 module.exports = function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method === "GET") {
     return res.status(200).json(applications);
   }
 
   if (req.method === "POST") {
     const newApp = {
-      id: applications.length + 1,
       ...req.body,
+      id: Math.max(...applications.map((a) => a.id)) + 1,
+      activities: [
+        {
+          id: 1,
+          type: "created",
+          timestamp: new Date().toISOString(),
+          note: "Application submitted",
+        },
+      ],
     };
-
     applications.push(newApp);
     return res.status(201).json(newApp);
   }
 
   return res.status(405).json({ message: "Method not allowed" });
-}
+};
